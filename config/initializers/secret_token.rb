04b79f380f6c2adde35e957b764e0b8f48ac24d8
Brad Hubbard <brad.hubbard77@gmail.com>
2014-07-30 20:32:07 -0700

    initial commit on project ellison

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectEllison::Application.config.secret_key_base = 'fb7454b7de3cd1a100b4a949ce7a0414d33672dca7678f79d00ea3008ba1fcee0c6344f092d124dd553e3ab510f498080b70472b4210dbf7d80953a665ae365c'
